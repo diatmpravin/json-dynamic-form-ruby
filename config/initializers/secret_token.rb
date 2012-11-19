6491dff245d34eb5411a3359c6acd1ba0abe9a9a
diatmpravin <diatm.pravin.it.07.27@gmail.com>
2012-11-19 23:48:20 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsonDynamicFormRuby::Application.config.secret_token = 'e7a13794c2fe35229f8760204fb6f47413d39e288bfc3be3634e634256c33600be9b4dc7c40842907ae7168bc8b8dea8c37640215f21ae19a740a2efd7623b04'
